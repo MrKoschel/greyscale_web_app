faac329985f9cb4eb6b3e8d5da4699c99f624a59
Piotr Walkowiak <piotrwalkowiak16@wp.pl>
2024-11-07 22:21:19 +0100

    1.1 Added upload button

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from PIL import Image
 
-camera_image = st.camera_input("Camera")
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("Camera")
 
 if camera_image:
     img = Image.open(camera_image)
     gray_img = img.convert("L")
     st.image(gray_img)
+
+uploaded_file = st.file_uploader("Upload image")
+if uploaded_file:
+    img = Image.open(uploaded_file)
+    uploaded_file_grey = img.convert("L")
+    st.image(uploaded_file_grey)
